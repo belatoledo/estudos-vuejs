bac03bfb016cbd2438b48b70faba0e565fbee8ac
Izabela <izabela.toledo@keycash.io>
2022-09-30 18:43:32 -0300

    Props validations

--- vue-zero-advanced/vue-intermediary/my-app/src/App.vue
+++ vue-zero-advanced/vue-intermediary/my-app/src/App.vue
@@ -17,7 +17,7 @@ export default {
   },
   data() {
     return {
-      lastname: '',
+      lastname: 'Toledo',
       desc: ''
     }
   },
--- vue-zero-advanced/vue-intermediary/my-app/src/components/User.vue
+++ vue-zero-advanced/vue-intermediary/my-app/src/components/User.vue
@@ -1,13 +1,19 @@
 <template>
  <div id="user">
-  <h2>Author: <span>{{name}} {{lastname}}</span></h2>
+  <h2>Author: <span>{{name}} {{lastname.split('')}}</span></h2>
  </div>
 </template>
 
 <script>
 export default {
  name: 'User',
- props: ['lastname'],
+ props: {
+  lastname: {
+   type: String,
+   required: true
+  }
+ },
+ //without validation → props: ['lastname'],
  data() {
   return {
    name: 'Izabela'
