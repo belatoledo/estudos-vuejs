2f97ed3453caddc154f1f460421b64dff87bdbf8
belatoledo <it.amancio@gmail.com>
2023-02-11 10:11:31 -0300

    style: app-bar

--- todo-list/src/App.vue
+++ todo-list/src/App.vue
@@ -23,10 +23,44 @@
       </v-list>
     </v-navigation-drawer>
 
-    <v-app-bar app clipped-left>
+    <v-app-bar
+      app
+      clipped-left
+      color="#fcb69f"
+      dark
+      prominent
+      src="./assets/bg-img.jpg"
+    >
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"
+          gradient="to top right, rgba(55,236,186,.7), rgba(25,32,72,.7)"
+        ></v-img>
+      </template>
+
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-app-bar-title>Welcome!</v-app-bar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <!-- <v-app-bar app clipped-left>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title>Menu</v-toolbar-title>
-    </v-app-bar>
+    </v-app-bar> -->
 
     <v-main>
       <router-view />
